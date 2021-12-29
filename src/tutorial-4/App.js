2fe57ff8ed94c69e07242043408068f8cea21330
Petr <kim.pyoter2017@yandex.ru>
2021-12-29 20:55:12 +0300

    task-5

--- src/tutorial-4/App.js
+++ src/tutorial-4/App.js
@@ -1,65 +1,88 @@
 import React from "react"
 
 import './App.css';
-
-// function Clickw() {
-//   setCount(count + { text: "fffffffffffffasdfas" })
-// }
+import EmptyBlock from "./components/EmptyBlock";
+import Phrase from "./components/Phrase";
 
 function App() {
-  const tabs = [
-    {
-      id: 1,
-      title: 'Сколько всего мест в доме?',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quos nemo corporis velit culpa veritatis asperiores deserunt, commodi ipsum at? Esse quibusdam dignissimos recusandae enim. Eaque expeditaeum provident totam!',
-    },
-    {
-      id: 2,
-      title: 'Самая дорогая квартира?',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fuga deserunt aliquid voluptatum ad, molestiae dicta officiis animi ummollitia, reiciendis a cum ratione veritatis cupiditate voluptatem.Consectetur, exercitationem magnam.',
-    },
-    {
-      id: 3,
-      title: 'Могу ли я отменить бронирование?',
-      description: 'Да, вы можете отменить забронированную квартиру в течение 3 дней.',
-    },
-    {
-      id: 4,
-      title: 'Можно ли купить квартиру?',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est commodi itaque ratione voluptatem dolores iusto aperiam adipisci incidunt perspiciatis ullam! Repellat accusamus rerum excepturi minus delectus? Consequatur libero nemo alias.',
-    },
+  const adjectivesArr = ["абсолютный",
+    "адский",
+    "азартный",
+    "активный",
+    "ангельский",
+    "астрономический",
+    "баснословный",
+    "безбожный",
+    "безбрежный",
+    "безвозвратный",
+    "безграничный",
+    "бездонный",
+    "бездушный",
+    "безжалостный",
+    "замечательно",
+    "замечательный",
+    "записной",
+    "запредельный",
+    "заядлый",
+    "звериный",
+    "зверский",
+    "зеленый",
+    "злой",
+    "злостный",
+    "значительный",
+    "неоспоримый",
+    "неотразимый",
+    "неоценимый",
+    "непередаваемый"
+  ];
+  const nounsArr = [
+    "лгун",
+    "день",
+    "конь",
+    "олень",
+    "человек",
+    "программист",
+    "ребёнок",
+    "конец",
+    "город",
+    "дурак"
   ];
 
 
-  const [active, setActive] = React.useState("")
+  const [active, setActive] = React.useState([])
 
-  function TabClick(id) {
-    setActive(id);
+  function TabClick() {
+    let word1 = Math.floor(Math.random() * adjectivesArr.length);
+    let word2 = Math.floor(Math.random() * adjectivesArr.length);
+    let word3 = Math.floor(Math.random() * nounsArr.length);
+
+    setActive((prev) =>
+      [...prev, `${adjectivesArr[word1]} ${adjectivesArr[word2]} ${adjectivesArr[word3]}`,
+      ]);
   }
 
+
+  const clear = () => {
+    setActive([])
+  }
+
+
   return (
-    <div id="app">
-
-      <div className="app-container">
-        <h1 className="app-title">FAQ</h1>
-        <div className="app-tabs">
-          {tabs.map(tab => {
-            return (
-              <div className={`tab ${active === tab.id ? "active" : ""}`}
-                key={tab.title}>
-
-              </div>
-            )
-          })}
-        </div>
+    <div className="wrapper">
+      <div >
+        {active && active.map((text, index) => <Phrase key={index} text={text}> /</Phrase>)}
       </div>
-    </div>
+      {!active.length && <EmptyBlock />}
+      <button
+        onClick={TabClick}
+        className="btn btn_generate">
+        Сгенерировать
+      </button>
+      <button onClick={clear} className="btn btn_clear">Очистить</button>
+
+    </div >
 
   )
 }
-
 
 export default App;
