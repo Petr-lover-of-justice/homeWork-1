537ee51175c5375baddaab424c701f1f5c0ac205
Petr <kim.pyoter2017@yandex.ru>
2022-01-16 09:03:48 +0300

    tutorial-7

--- src/tutorial-4/App.js
+++ src/tutorial-4/App.js
@@ -1,51 +1,64 @@
-import React from "react";
-import './App.css';
-import Form from "./components/form";
-import List from "./components/list";
+import { Nav, Navbar, Row, Col, Card } from "react-bootstrap";
+import { Article } from "./components/Article";
+export default function App() {
+  const { pathname } = window.location;
 
 
-
-// Imported Components below
-
-function App() {
-  const [comments, setComments] = React.useState([]);
-
-  const onSave = (name, email, text) => {
-    const newData = {
-      fullName: name,
-      email: email,
-      createdAt: inputChangedAt,
-      text: text,
-      id: Math.random()
-    }
-    setComments([...comments, newData])
-  }
-
-  const onRemove = (id) => {
-    const newArr = comments.filter((i) => i.id !== id);
-    localStorage.setItem('comments', JSON.stringify(newArr))
-    setComments(newArr)
-  }
-
-  const inputChangedAt = new Date().toLocaleTimeString("ru-RU", { year: "numeric", day: "numeric", month: "numeric" })
-  React.useEffect(() => {
-    if (localStorage.getItem('comments')) {
-      setComments(JSON.parse(localStorage.getItem('comments')))
-    }
-  }, [])
-
-  React.useEffect(() => {
-    if (comments.length) {
-      localStorage.setItem('comments', JSON.stringify(comments))
-    }
-  }, [comments])
-
+  const postId = pathname.split('/').at(-1);
   return (
-    <div>
-      <List comments={comments} onRemove={onRemove} />
-      <Form onSave={onSave} />
+    <div className="App">
+      <header>
+        <h2>
+          <a href="/">React Blog</a>
+        </h2>
+        <Nav variant="pills" defaultActiveKey="/">
+          <Nav.Item>
+            <Nav.Link eventKey="/home" to="/">
+              Главная
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="/home" to="/about">
+              Обо мне
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="/home" to="/profile">
+              Профиль
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </header>
+      {pathname === "/" && (
+        <Row xs={1} md={3} className="g-4">
+          <Col>
+            <Card>
+              <Card.Img variant="top" src="https://via.placeholder.com/150x150" />
+              <Card.Body>
+                <Card.Title>
+                  <a href="/post/1">Card title</a>
+                </Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      )}
+      {pathname === `/post/${postId}` && (
+        <Article id={postId} />
+      )}
+      {pathname === "/about" && (
+        <Card>
+          <Card.Body>Это мой личный сайт!</Card.Body>
+        </Card>
+      )}
+      <br />
+      <Navbar bg="light" style={{ paddingLeft: 20 }}>
+        <Navbar.Brand href="#home">My site (c) 2021</Navbar.Brand>
+      </Navbar>
     </div>
-  )
+  );
 }
-
-export default App;
